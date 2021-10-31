cb2345f0b74ec98aecfd2fe40d8a2b097b3d0b64
Marian <marian.caikovski@gmail.com>
2021-10-31 21:35:12 +0100

    first

--- js/np/module1.js
+++ js/np/module1.js
@@ -2,7 +2,7 @@ import val2 from './module2.js';
 import val3 from './module3.js';
 
 const total=Date.now()-t0;
- document.querySelector('div').replaceChildren(total );
+ document.querySelector('div').replaceChildren(total+"*" );
  
 window.parent.postMessage(total,"*");
 console.log(val2+val3);
